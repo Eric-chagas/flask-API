654f75d027164fea35de2612fb4514a79342742e
Eric-chagas <eric_chagas12@outlook.com>
2022-01-20 23:25:16 -0300

    Added List of hotels endpoint

--- resources/Hotel.py
+++ resources/Hotel.py
@@ -1,6 +1,33 @@
 from flask_restful import Resource
 
+HotelList = [
+    {'Hotel_id': 0,
+     'name': 'Gran Hotel',
+     'stars': 3.5,
+     'dailyFee': 245,
+     'city': 'Los Angeles'
+     },
+    {'Hotel_id': 1,
+     'name': 'Minor Hotel',
+     'stars': 5,
+     'dailyFee': 1200,
+     'city': 'Springfield'
+     },
+    {'Hotel_id': 2,
+     'name': 'My Own Private',
+     'stars': 1.3,
+     'dailyFee': 12,
+     'city': 'Idaho'
+     },
+    {'Hotel_id': 3,
+     'name': 'Roam if you want to',
+     'stars': 2.8,
+     'dailyFee': 100,
+     'city': 'Connecticut'
+     }
+]
+
 
 class Hotels(Resource):
     def get(self):
-        return {'hotels': 'My Hotels'}
+        return {'hotels': HotelList}
